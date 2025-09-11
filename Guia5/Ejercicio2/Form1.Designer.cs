c2149437a5965bc078cbba9c6f2750d574744886
Facundo Citera <facundojesus10@hotmail.com>
2025-09-11 16:33:04 -0300

    Diez mil nombres

---
+++ Guia5/Ejercicio2/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Ejercicio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lsbResultado = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // lsbResultado
+            // 
+            lsbResultado.FormattingEnabled = true;
+            lsbResultado.ItemHeight = 15;
+            lsbResultado.Location = new Point(12, 12);
+            lsbResultado.Name = "lsbResultado";
+            lsbResultado.Size = new Size(346, 409);
+            lsbResultado.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(387, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 54);
+            button1.TabIndex = 1;
+            button1.Text = "Generar Listado";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(475, 450);
+            Controls.Add(button1);
+            Controls.Add(lsbResultado);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Ejercicio2";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox lsbResultado;
+        private Button button1;
+    }
+}
---
+++ Guia5/Ejercicio2/Form1.cs
@@ -0,0 +1,35 @@
+using Microsoft.VisualBasic.Devices;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Ejercicio2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random azar = new Random();
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> Apellidos = new List<string> { "Hernandez", "Saavedra", "Acosta","Jacob", "Heinze","Fischer","Campos" };
+            List<string> Nombres = new List<string> { "Adriana", "Elizabet", "José", "María", "Ernesto", "Sebastian", "Julio", "Ester", "Ariel", "Betiana", "Silvina", "Ana", "Leandro", "Ayelen", "Daniela", "Miguel" };
+
+            StringBuilder listado = new StringBuilder();
+            for (int n = 0; n < 10000; n++)
+            {
+                int o = azar.Next(Nombres.Count);
+                int p = azar.Next(Apellidos.Count);
+
+                listado.AppendLine($"{Apellidos[p]}, {Nombres[o]}");
+            }
+
+            lsbResultado.Items.AddRange(listado.ToString().Split("\n"));
+        }
+
+    }
+    
+}
