99b0cbd9abd7c91e7e8c32ae0f342e7e0fc7b8d9
FacuJesus <facundojesus10@hotmail.com>
2025-09-14 11:13:58 -0300

    Texto Enriquecido

---
+++ Guia5/Ejercicio4/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Ejercicio4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tbPalabra = new TextBox();
+            btnBuscar = new Button();
+            richTextBox1 = new RichTextBox();
+            label1 = new Label();
+            lbCantidad = new Label();
+            SuspendLayout();
+            // 
+            // tbPalabra
+            // 
+            tbPalabra.Location = new Point(12, 12);
+            tbPalabra.Name = "tbPalabra";
+            tbPalabra.Size = new Size(505, 23);
+            tbPalabra.TabIndex = 0;
+            // 
+            // btnBuscar
+            // 
+            btnBuscar.Location = new Point(544, 12);
+            btnBuscar.Name = "btnBuscar";
+            btnBuscar.Size = new Size(75, 23);
+            btnBuscar.TabIndex = 1;
+            btnBuscar.Text = "Buscar";
+            btnBuscar.UseVisualStyleBackColor = true;
+            btnBuscar.Click += btnBuscar_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(12, 53);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(505, 313);
+            richTextBox1.TabIndex = 3;
+            richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(189, 383);
+            label1.Name = "label1";
+            label1.Size = new Size(146, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Cantidad de coincidencias";
+            // 
+            // lbCantidad
+            // 
+            lbCantidad.BackColor = SystemColors.ActiveCaption;
+            lbCantidad.Location = new Point(219, 407);
+            lbCantidad.Name = "lbCantidad";
+            lbCantidad.Size = new Size(76, 24);
+            lbCantidad.TabIndex = 5;
+            lbCantidad.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(647, 450);
+            Controls.Add(lbCantidad);
+            Controls.Add(label1);
+            Controls.Add(richTextBox1);
+            Controls.Add(btnBuscar);
+            Controls.Add(tbPalabra);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Ejercicio4";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox tbPalabra;
+        private Button btnBuscar;
+        private RichTextBox richTextBox1;
+        private Label label1;
+        private Label lbCantidad;
+    }
+}
---
+++ Guia5/Ejercicio4/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Ejercicio4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+
+
+            if (tbPalabra.Text != "")
+            {
+                // 1. Inicializar frase
+                string frase = "La casa es grande y la casa tiene un jardín. La casa también tiene un perro en la casa.";
+
+                // 2. Palabra a resaltar
+                string palabraAResaltar = tbPalabra.Text;
+
+                // 3. Usar Regex → reemplazar palabra por versión en RTF con negrita
+                string patron = Regex.Escape(palabraAResaltar);
+                string textoRTF = Regex.Replace(frase, patron, $@"{{\b {palabraAResaltar}\b0}}", RegexOptions.IgnoreCase);
+
+
+                // 4. Muestro en RichTextBox
+                richTextBox1.Rtf = $@"{{\rtf1\ansi {textoRTF}}}";
+
+                //5. Mostrar cantidad de coincidencias
+                MatchCollection matches = Regex.Matches(frase, patron, RegexOptions.IgnoreCase);
+                lbCantidad.Text = $"{matches.Count}";
+            }
+            else
+            {
+                MessageBox.Show("Debes ingresar una palabra a buscar");
+            }
+
+
+        }
+
+
+    }
+}
